c61f61e042ebfc102090eb21e668c491bf1f17b9
Alorf <arthur.lorfevre@outlook.be>
2024-08-22 11:08:11 +0200

    Fix system logger

--- TwitchDropsBot.Core/Utilities/SystemLogger.cs
+++ TwitchDropsBot.Core/Utilities/SystemLogger.cs
@@ -1,24 +1,46 @@
+using TwitchDropsBot.Core.Object;
+
 namespace TwitchDropsBot.Core;
 
-public class SystemLogger : Logger
+public class SystemLogger
 {
+
     public static void Log(string message)
     {
-        Log(message);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"[{DateTime.Now}] LOG : {message}");
+        Console.ResetColor();
     }
-    
+
     public static void Error(string message)
     {
-        Error(message);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"[{DateTime.Now}] ERROR : {message}");
+        Console.ResetColor();
     }
-    
+
     public static void Error(System.Exception exception)
     {
-        Error(exception);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"[{DateTime.Now}] ERROR : {exception.Message}\n{exception.StackTrace}");
+
+        foreach (var data in exception.Data)
+        {
+            Console.WriteLine(data);
+        }
+        
+        // print inner exception
+        if (exception.InnerException != null)
+        {
+            Console.WriteLine(exception.InnerException);
+        }
+        Console.ResetColor();
     }
-    
+
     public static void Info(string message)
     {
-        Info(message);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"[{DateTime.Now}] INFO : {message}");
+        Console.ResetColor();
     }
 }

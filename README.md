f33a4413ed123c514c6b7dc71a45fbd1d586c4b8
Alorf <arthur.lorfevre@outlook.be>
2025-02-18 18:08:15 +0100

    feat: multiple accounts handling, add --add-account parameter

--- README.md
+++ README.md
@@ -9,27 +9,33 @@ The main goal of this project is to help users claim their Twitch drops when the
 > If you have any questions, feel free to open a detailed issue.
 
 ## Features
+
 - [x] Support for **Campaigns** (Rust) and **Reward Campaigns** (Minecraft items)
 - [x] Manage multiple Twitch accounts
 - [x] Priority list
 - [x] Discord Webhook support
-- [x] Easy GQL updates using [Postman](https://www.postman.com/) *(see the Postman section)*
+- [x] Easy GQL updates using [Postman](https://www.postman.com/) _(see the Postman section)_
 
 ### GUI Version
+
 - [x] Tray icon
 - [x] Manage your priority list
 - [x] Inventory section showing your inventory and current campaigns
 - [x] Button to add new accounts
 
 ### Console Version
+
 - [x] Fully functional!
-- [ ] Add account feature (upcoming)
+- [x] Add account feature (start the console with the parameter `--add-account`)
 
 ## Getting Started
+
 ### Compiled Version
+
 Whenever I consider a new version ready for release, I publish it on the [Releases](https://github.com/Alorf/TwitchDropsBot/releases) page.
 
 #### Usage
+
 1. Download the bot.
 2. Launch the bot.
 3. Log in.
@@ -39,11 +45,13 @@ Whenever I consider a new version ready for release, I publish it on the [Releas
 7. You're done!
 
 #### Updates
+
 1. Download the bot.
 2. Copy and paste your config file.
 3. You're done!
 
 ## Postman
+
 Twitch frequently changes the hashes for GQL operations used by the bot.  
 I’ve created a Postman collection that contains all the required GQL requests.  
 If the bot stops working, you can replace the hashes using this collection.  
@@ -53,6 +61,7 @@ Once updated, simply export your collection and replace the old one.
 > The updated collection must have the same name as the original one.
 
 ## Contributing
+
 This app was built using [Visual Studio](https://visualstudio.microsoft.com).  
 We welcome contributions! To contribute, follow these steps:
 
@@ -63,4 +72,5 @@ We welcome contributions! To contribute, follow these steps:
 5. Open a pull request.
 
 ## License
+
 This project is licensed under the MIT License. See the [LICENSE](https://github.com/Alorf/TwitchDropsBot/blob/master/LICENSE.txt) file for details.
--- TwitchDropsBot.Console/Program.cs
+++ TwitchDropsBot.Console/Program.cs
@@ -7,54 +7,40 @@ using TwitchDropsBot.Core.Utilities;
 
 AppConfig config = AppConfig.Instance;
 
+if (args.Length > 0 && args[0] == "--add-account" && config.Users.Count != 0)
+{
+    do
+    {
+        SystemLogger.Info("Do you want to add another account? (Y/N)");
+        string answer = Console.ReadLine();
+        if (answer.ToLower() == "n")
+        {
+            break;
+        }
+
+        await AuthDeviceAsync();
+    } while (true);
+}
+
 while (config.Users.Count == 0)
 {
     SystemLogger.Info("No users found in the configuration file.");
     SystemLogger.Info("Login process will start.");
 
     await AuthDeviceAsync();
-    config = AppConfig.Instance;
 }
 
 TimeSpan waitingTime;
+List<Task> botTasks = new List<Task>();
+
 foreach (ConfigUser user in config.Users)
 {
     TwitchUser twitchUser = new TwitchUser(user.Login, user.Id, user.ClientSecret, user.UniqueId);
     twitchUser.DiscordWebhookURl = config.WebhookURL;
-
-    Bot bot = new Bot(twitchUser);
-
-    while (true)
-    {
-        try
-        {
-            await bot.StartAsync();
-            waitingTime = TimeSpan.FromSeconds(20);
-        }
-        catch (NoBroadcasterOrNoCampaignLeft ex)
-        {
-            twitchUser.Logger.Info(ex.Message);
-            twitchUser.Logger.Info("Waiting for 5 minutes before trying again.");
-            waitingTime = TimeSpan.FromMinutes(5);
-        }
-        catch (StreamOffline ex)
-        {
-            twitchUser.Logger.Info(ex.Message);
-            twitchUser.Logger.Info("Waiting for 5 minutes before trying again.");
-            waitingTime = TimeSpan.FromMinutes(5);
-        }
-        catch (Exception ex)
-        {
-            twitchUser.Logger.Error(ex);
-            waitingTime = TimeSpan.FromMinutes(5);
-        }
-        twitchUser.StreamURL = null;
-        twitchUser.Status = BotStatus.Idle;
-
-        await Task.Delay(waitingTime);
-    }
+    botTasks.Add(StartBot(twitchUser));
 }
 
+await Task.WhenAll(botTasks);
 static async Task AuthDeviceAsync()
 {
     var jsonResponse = await AuthSystem.GetCodeAsync();
@@ -82,4 +68,50 @@ static async Task AuthDeviceAsync()
     config.Users.Add(user);
 
     config.SaveConfig();
+}
+
+static Task StartBot(TwitchUser twitchUser)
+{
+    Bot bot = new Bot(twitchUser);
+    TimeSpan waitingTime;
+    twitchUser.CancellationTokenSource = new CancellationTokenSource();
+    return Task.Run(async () =>
+    {
+        while (true)
+        {
+            try
+            {
+                await bot.StartAsync();
+                waitingTime = TimeSpan.FromSeconds(20);
+            }
+            catch (NoBroadcasterOrNoCampaignLeft ex)
+            {
+                twitchUser.Logger.Info(ex.Message);
+                twitchUser.Logger.Info("Waiting 5 minutes before trying again.");
+                waitingTime = TimeSpan.FromMinutes(5);
+            }
+            catch (StreamOffline ex)
+            {
+                twitchUser.Logger.Info(ex.Message);
+                twitchUser.Logger.Info("Waiting 5 minutes before trying again.");
+                waitingTime = TimeSpan.FromMinutes(5);
+            }
+            catch (OperationCanceledException ex)
+            {
+                twitchUser.Logger.Info(ex.Message);
+                twitchUser.CancellationTokenSource = new CancellationTokenSource();
+                waitingTime = TimeSpan.FromSeconds(10);
+            }
+            catch (Exception ex)
+            {
+                twitchUser.Logger.Error(ex);
+                waitingTime = TimeSpan.FromMinutes(5);
+            }
+
+            twitchUser.StreamURL = null;
+            twitchUser.Status = BotStatus.Idle;
+
+            await Task.Delay(waitingTime);
+        }
+    });
 }
